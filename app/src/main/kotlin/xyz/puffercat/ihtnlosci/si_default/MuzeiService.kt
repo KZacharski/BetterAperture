ea44b5e68869297a61dd3345951945bfd4dcfd6b
Kacper Zacharski <puffercatt@gmail.com>
2022-12-15 21:12:42 +0100

    Finish work on the first single icon version

---
+++ app/src/main/kotlin/xyz/puffercat/ihtnlosci/si_default/MuzeiService.kt
@@ -0,0 +1,5 @@
+package xyz.puffercat.ihtnlosci.si_default
+
+import dev.jahir.frames.muzei.FramesArtProvider
+
+class MuzeiService : FramesArtProvider()
---
+++ app/src/main/kotlin/xyz/puffercat/ihtnlosci/si_default/ZooperProvider.kt
@@ -0,0 +1,5 @@
+package xyz.puffercat.ihtnlosci.si_default
+
+import dev.jahir.kuper.data.providers.ZooperTemplateProvider
+
+class ZooperProvider : ZooperTemplateProvider()
